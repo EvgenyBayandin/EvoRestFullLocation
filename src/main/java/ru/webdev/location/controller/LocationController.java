cd1b60a82a1ecae4f75c4ff7dc37e302378b1647
Evgeny Bayandin <5087081@gmail.com>
2024-05-30 18:48:25 +0300

    Update LocationController, added variables geoUrl, geoLimit, geoKey, RestTemplate, modify @PostMapping save()

--- src/main/java/ru/webdev/location/controller/LocationController.java
+++ src/main/java/ru/webdev/location/controller/LocationController.java
@@ -1,15 +1,15 @@
 package ru.webdev.location.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import ru.webdev.location.model.Location;
 import ru.webdev.location.repository.LocationRepository;
 
@@ -17,8 +17,22 @@ import ru.webdev.location.repository.LocationRepository;
 @RequestMapping("/location")
 public class LocationController {
 
+    @Value("${geo.url}")
+    private String geoUrl;
+
+    @Value("${geo.limit}")
+    private int geoLimit;
+
+    @Value("${geo.key}")
+    private String geoKey;
+
     @Autowired
     private LocationRepository repository;
+
+    RestTemplate restTemplate= new RestTemplate();
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping
     public Iterable<Location> findAll() {
@@ -31,9 +45,32 @@ public class LocationController {
     }
 
     @PostMapping
-    public ResponseEntity<Location> save(@RequestBody Location location) {
-        return repository.findById(location.getId()).isPresent()
-                ? new ResponseEntity(repository.findById(location.getId()), HttpStatus.BAD_REQUEST)
-                : new ResponseEntity(repository.save(location), HttpStatus.CREATED);
+    public ResponseEntity<Location> save(@RequestBody Location location) throws JsonProcessingException {
+        // Проверяем, существует ли объект в базе данных
+        if (repository.findByTitle(location.getTitle()).isPresent()) {
+            // Возвращаем ошибку, если объект уже существует
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        // Запрос к внешнему сервису для получения координат
+        String url = geoUrl + location.getTitle() + "&limit=" + geoLimit + "&appid=" + geoKey;
+        String jsonResponse = restTemplate.getForObject(url, String.class);
+
+        // Распарсим ответ
+        JsonNode rootNode = objectMapper.readTree(jsonResponse);
+        // Получаем первый элемент
+        JsonNode locationNode = rootNode.get(0);
+
+        // Извлечем координаты
+        Float latitude = locationNode.get("lat").floatValue();
+        Float longitude = locationNode.get("lon").floatValue();
+
+        // Сохраняем объект в репозиторий
+        Location newLocation  = new Location(location.getTitle(), latitude, longitude);
+        repository.save(newLocation);
+
+        // Возвращаем успешный ответ с созданным объектом
+        return new ResponseEntity<>(newLocation, HttpStatus.CREATED);
     }
+
 }
