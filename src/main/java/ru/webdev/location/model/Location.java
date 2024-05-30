df3292b539e96c014b9181dba00821902679d55f
Evgeny Bayandin <5087081@gmail.com>
2024-05-30 18:45:57 +0300

    Update Location

--- src/main/java/ru/webdev/location/model/Location.java
+++ src/main/java/ru/webdev/location/model/Location.java
@@ -4,6 +4,7 @@ package ru.webdev.location.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
