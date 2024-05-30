054ef74ebd27b2ac9aaac70451eea5879fa286c7
Evgeny Bayandin <5087081@gmail.com>
2024-05-30 11:30:40 +0300

    Added package Repository, added LocationRepository.java and annotations for class.

---
+++ src/main/java/ru/webdev/location/repository/LocationRepository.java
@@ -0,0 +1,10 @@
+package ru.webdev.location.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.webdev.location.model.Location;
+
+@Repository
+public interface LocationRepository extends CrudRepository<Location, Integer> {
+
+}
